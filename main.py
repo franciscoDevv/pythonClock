d912d7afa839f6bf05c186cde68be79e732c91a0
Francisco Muñoz <77551844+franciscomunozz@users.noreply.github.com>
2023-01-28 16:55:16 -0300

    translated the data recieved from time library with the translate library
    if u want to try this code install with pip install translate

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import random
 import os
 import time
+from translate import Translator
 
 list = ["¡Ten un buen día!", "Si te caes tres veces, levántate cuatro", "Prohibido rendirse", "Lucha por ti mismo, por lo que eres, por quien eres", "Comienza a ser ahora, lo que serás de ahora en adelante"]
 s = random.choice(list)
 
 start_time = time.time()
 
+d = time.strftime("%A")
+m = time.strftime("%B")
+translator = Translator(to_lang="es")
+day = translator.translate(d)
+month = translator.translate(m)
 while True:
     current_time = time.time()
     if current_time - start_time >= 1:
         start_time = current_time
         os.system('clear')
-        print(time.strftime("%d, %A %B %X %p " + s))
+        print(time.strftime(day + " %d " + "de " + month + " %X %p \n" + s))
