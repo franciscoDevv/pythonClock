38df8221e68949c3dd62547fe5274dbc8fbc8c58
Francisco Muñoz <77551844+franciscomunozz@users.noreply.github.com>
2023-01-29 17:13:52 -0300

    added big text and timezone

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import random
 import os
 import time
+import datetime
 from translate import Translator
+from termcolor import colored
+from datetime import datetime
+from pyfiglet import Figlet
+import pytz
+
+f = Figlet(font='big')
 
 list = ["¡Ten un buen día!", "Si te caes tres veces, levántate cuatro", "Prohibido rendirse", "Lucha por ti mismo, por lo que eres, por quien eres", "Comienza a ser ahora, lo que serás de ahora en adelante"]
 s = random.choice(list)
@@ -13,9 +20,13 @@ m = time.strftime("%B")
 translator = Translator(to_lang="es")
 day = translator.translate(d)
 month = translator.translate(m)
+
 while True:
-    current_time = time.time()
+    current_time = time.time()  
     if current_time - start_time >= 1:
         start_time = current_time
         os.system('clear')
-        print(time.strftime(day + " %d " + "de " + month + " %X %p \n" + s))
+        timezone = pytz.timezone('UTC')
+        print(f.renderText('Reloj')) # Using Figlet for text art
+        print(time.strftime(colored(day, 'yellow') + " %d " + "de " + month + " %X %p \n" + colored(s, 'red')))
+        print("Zona horaria: " + str(datetime.now(timezone).strftime('%Z')))
